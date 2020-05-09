dea127587077cbfe005f65ac047a62d6570a43e9
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-05-10 01:07:14 +0800

    build: use @modyqyw/stylelint-config-scss

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,25 +1,4 @@
 module.exports = {
-  extends: [
-    "stylelint-config-twbs-bootstrap/scss",
-  ],
-  rules: {
-    "declaration-no-important": null,
-    "selector-no-qualifying-type": [
-      true,
-      {
-        ignore: [
-          "attribute",
-          "class",
-        ],
-      },
-    ],
-    "selector-type-no-unknown": null,
-    "unit-no-unknown": null,
-  },
-  ignoreFiles: [
-    "*.sh*",
-    "*.js*",
-    "*.md*",
-    "LICENSE",
-  ],
+  extends: ["@modyqyw/stylelint-config-scss"],
+  ignoreFiles: ["*.sh*", "*.js*", "*.md*", "LICENSE"],
 };
