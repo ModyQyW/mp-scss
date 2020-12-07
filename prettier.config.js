db8ee12078637f38a01903d1f266aab7ea438e48
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-12-07 14:40:59 +0800

    chore: update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,14 @@ const config = require('@modyqyw/fabric/prettier');
 
 module.exports = {
   ...config,
+  overrides: [
+    {
+      files: ['*.css', '*.less', '*.scss'],
+      options: {
+        printWidth: 150,
+        singleQuote: false,
+        trailingComma: 'none',
+      },
+    },
+  ],
 };
